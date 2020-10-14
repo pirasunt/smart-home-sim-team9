972970582b24046265397114e3e4f3d8022d9acf
Pira <31664874+pirasunt@users.noreply.github.com>
2020-10-14 17:37:15 -0400

    Added Mutators for UserProfile + Exception
    - UserProfiles can only be modified through the environment (or any class that is part of the Context package as the mutators are package-scoped)
    
    - All UserProfile Mutators return a deep copy and are then replaced in the Environment ArrayList. This is for encapsulation purposes
    
    - Created a checked Exception class (NonExistantUserProfileException) for when a modification is made to a deleted or non-existant profile

--- src/Context/Environment.java
+++ src/Context/Environment.java
@@ -2,6 +2,7 @@ package Context;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.UUID;
 
 public class Environment {
 
@@ -30,5 +31,65 @@ public class Environment {
         this.currentTime = newTime;
 
     }
+
+    /**
+     * Returns a deep copy of all the registered user profiles
+     * @return UserProfile Array of all registered user profile in the environment
+     */
+    public UserProfile[] getAllUserProfiles(){
+        UserProfile[] up = new UserProfile[this.userProfileList.size()];
+
+        for(int i =0; i< up.length ; i++){
+            up[i] = new UserProfile(this.userProfileList.get(i));
+        }
+        return up;
+    }
+
+
+    public void modifyProfileLocation(UserProfile profile, UUID roomID) {
+
+        try {
+            updateProfileEntry(profile.modifyLocation(roomID));
+        }
+        catch(NonExistantUserProfileException e) {
+            System.err.println(e.getMessage()); //Return some sort of error window?
+        }
+    }
+
+    public void editProfileName(UserProfile profile, String newName) {
+
+        try {
+            updateProfileEntry(profile.modifyName(newName));
+        }
+        catch(NonExistantUserProfileException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+
+    /**
+     * Internal helper method that finds and updates a current UserProfile object in the ArrayList attribute
+     * @param updatedProfile UserProfile object with updated attributes
+     */
+    private void updateProfileEntry(UserProfile updatedProfile) throws NonExistantUserProfileException {
+
+        boolean existingProfile = false;
+        int index = -1;
+        for(int i = 0; i < this.userProfileList.size(); i++) {
+            if(this.userProfileList.get(i).getProfileID() == updatedProfile.getProfileID()) {
+                existingProfile = true;
+                index = i;
+            }
+        }
+
+        if(existingProfile && index >= 0) {
+            this.userProfileList.set(index, updatedProfile);
+        } else {
+            throw new NonExistantUserProfileException("UserProfile " + updatedProfile.getProfileID() + " with name " + updatedProfile.getName() + "does not exist or has been deleted");
+        }
+
+    }
+
+
 
 }
--- src/Context/UserProfile.java
+++ src/Context/UserProfile.java
@@ -4,24 +4,64 @@ import common.profileType;
 
 import java.util.UUID;
 
-public class UserProfile {
-
+public class UserProfile{
+    private UUID profileID;
     private profileType type;
     private String name;
     private UUID roomID; //This value is actually a roomID and is where the profile user currently is
 
     public UserProfile(profileType type, String name, UUID id){
+        this.profileID = UUID.randomUUID();
         this.roomID = id;
         this.type = type;
         this.name = name;
     }
 
-    //TODO: HAVE THESE SETTERS RETURN EXCEPTIONS
-    public void modifyLocation(UUID id){
-        this.roomID = id;
+    //Copy Constructor
+    public UserProfile(UserProfile original){
+        this.profileID = original.profileID; //UUID immutable obj
+        this.roomID = original.roomID;
+        this.type = original.type;
+        this.name = original.name;
     }
 
-    public void editProfileName(String name) {
-        this.name = name;
+    /**
+     * Returns deep copy of a UserProfile object with the location attribute modified
+     * @param id The new Room ID to move the UserProfile to
+     * @return a Deep copy of the UserProfile with the roomID attribute modified
+     */
+     UserProfile modifyLocation(UUID id){
+        UserProfile temp = new UserProfile(this);
+        temp.roomID = id;
+        return temp;
     }
+
+    /**
+     * Returns deep copy of the UserProfile object with its name modified
+     * @param newName The new name of the UserProfile
+     * @return  a Deep copy of the UserProfile with the profile Name attribute modified
+     */
+    UserProfile modifyName(String newName) {
+         UserProfile temp = new UserProfile(this);
+         temp.name = newName;
+         return temp;
+    }
+
+    public UUID getProfileID() {
+        return this.profileID;
+    }
+
+    public profileType getProfileType() {
+        return this.type;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public UUID getRoomID() {
+        return this.roomID;
+    }
+
+
 }
