1c389ade01ed9e2f69f21b1cbf19adf3a2498c7c
Jason Botzas-Coluni <jason.botzas.coluni@gmail.com>
2020-10-17 18:16:44 -0400

    added javadoc

--- src/main/java/Views/Dash.java
+++ src/main/java/Views/Dash.java
@@ -16,14 +16,41 @@ import org.jdatepicker.impl.JDatePanelImpl;
 import org.jdatepicker.impl.JDatePickerImpl;
 import org.jdatepicker.impl.SqlDateModel;
 
+/**
+ * The type Dash.
+ */
 public class Dash extends JFrame {
+    /**
+     * The Stop simulation button.
+     */
     JButton stopSimulationButton;
+    /**
+     * The Edit action button.
+     */
     JButton editActionButton;
+    /**
+     * The Tabbed pane 1.
+     */
     JTabbedPane tabbedPane1;
+    /**
+     * The P 1.
+     */
     JPanel p1;
+    /**
+     * The Sp 1.
+     */
     JScrollPane sp1;
+    /**
+     * The Sp 2.
+     */
     JScrollPane sp2;
+    /**
+     * The Tab 2.
+     */
     JPanel Tab2;
+    /**
+     * The Tab 1.
+     */
     JPanel Tab1;
     private JComboBox comboBox1;
     private JComboBox comboBox2;
@@ -37,6 +64,9 @@ public class Dash extends JFrame {
     private Boolean action1 = false;
     private Boolean action2 = false;
 
+    /**
+     * Instantiates a new Dash.
+     */
     public Dash() {
         stopSimulationButton.addActionListener(new ActionListener() {
             @Override
@@ -88,6 +118,9 @@ public class Dash extends JFrame {
         });
     }
 
+    /**
+     * Change date.
+     */
     public void ChangeDate() {
         SqlDateModel model = new SqlDateModel();
         Properties p = new Properties();
@@ -125,6 +158,9 @@ public class Dash extends JFrame {
         this.setVisible(true);
     }
 
+    /**
+     * Change time.
+     */
     public void ChangeTime() {
         setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
