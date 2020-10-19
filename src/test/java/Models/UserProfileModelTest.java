3bd3009f7b6b333548a0fc39d07398371e890c5d
Jason Botzas-Coluni <jason.botzas.coluni@gmail.com>
2020-10-19 15:14:17 -0400

    Added more unit tests from use cases.

---
+++ src/test/java/Models/UserProfileModelTest.java
@@ -0,0 +1,21 @@
+package Models;
+
+import Enums.profileType;
+import Views.Console;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class UserProfileModelTest {
+    @BeforeEach
+    public void initEach(){
+        Console c = new Console();
+    }
+    @Test
+    void modifyUserProfileName() {
+        UserProfileModel u = new UserProfileModel(profileType.ADULT, "Old", -1);
+        EnvironmentModel env = EnvironmentModel.createSimulation(new House(), u);
+        env.setCurrentUser(u);
+        env.editProfileName(u, "New");
+        assert env.getCurrentUser().getName() == "New";
+    }
+}
