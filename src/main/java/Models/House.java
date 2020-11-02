db889d9fbf7c56d25956c3e3a5bea0e6f6ff991c
patrickgdbt <patrickcgodbout@gmail.com>
2020-11-02 15:01:27 -0500

    Added methods to be used for automatic updates of windows by the system

--- src/main/java/Models/House.java
+++ src/main/java/Models/House.java
@@ -12,6 +12,38 @@ public class House {
   /** The Rooms in the house. */
   ArrayList<Room> rooms = new ArrayList<Room>();
 
+  public House() {
+    int room1ID = 1;
+    int room2ID = 2;
+    int room3ID = 3;
+    int room4ID = 4;
+    int room5ID = 5;
+    int room6ID = 6;
+    int room7ID = 7;
+    int room8ID = 8;
+    int room9ID = 9;
+
+    Room room1 = new Room("Room 1", new RoomWall(room4ID), new RoomWall(room2ID), new OutsideWall(), new WindowWall(1), room1ID);
+    Room room2 = new Room("Room 2", new RoomWall(room3ID), new WindowWall(2), new RoomWall(room7ID), new RoomWall(room1ID), room2ID);
+    Room room3 = new Room("Room 3", new WindowWall(3), new WindowWall(4), new RoomWall(room2ID), new Wall(), room3ID);
+    Room room4 = new Room("Room 4", new WindowWall(5), new Wall(), new RoomWall(room1ID), new RoomWall(room5ID), room4ID);
+    Room room5 = new Room("Room 5", new RoomWall(room6ID), new RoomWall(room4ID), new Wall(), new WindowWall(6), room5ID);
+    Room room6 = new Room("Room 6", new WindowWall(7), new WindowWall(), new RoomWall(room5ID), new OutsideWall(), room6ID);
+    Room room7 = new Room("Room 7", new RoomWall(room2ID), new RoomWall(room9ID), new RoomWall(room8ID), new WindowWall(), room7ID);
+    Room room8 = new Room("Room 8", new RoomWall(room7ID), new WindowWall(8), new WindowWall(9), new Wall(), room8ID);
+    Room room9 = new Room("Room 9", new Wall(), new WindowWall(10), new Wall(), new RoomWall(room7ID), room9ID);
+
+    rooms.add(room1);
+    rooms.add(room2);
+    rooms.add(room3);
+    rooms.add(room4);
+    rooms.add(room5);
+    rooms.add(room6);
+    rooms.add(room7);
+    rooms.add(room8);
+    rooms.add(room9);
+  }
+
   /**
    * Gets rooms in the house.
    *
--- src/main/java/Models/UserProfileModel.java
+++ src/main/java/Models/UserProfileModel.java
@@ -9,8 +9,7 @@ public class UserProfileModel {
   private final UUID profileID;
   private final ProfileType type;
   private String name;
-  private int
-      roomID; /* Indicates UserProfile's current room location (A value of -1 indicates that a room
+  private int roomID; /* Indicates UserProfile's current room location (A value of -1 indicates that a room
    has not been assigned to the UserProfile)*/
 
   /**
