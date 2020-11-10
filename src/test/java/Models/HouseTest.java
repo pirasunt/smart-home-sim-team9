d2f6b4454ae7f7a6762dd8c1889ffef82852f2f2
Pira <31664874+pirasunt@users.noreply.github.com>
2020-11-10 17:14:41 -0500

    Created Unit Test for Automatic Lights
    - Tests the observer

--- src/test/java/Models/HouseTest.java
+++ src/test/java/Models/HouseTest.java
@@ -1,7 +1,14 @@
 package Models;
 
+import Controllers.CoreController;
+import Custom.CustomXStream.CustomHouseXStream;
+import Enums.ProfileType;
 import Models.Walls.Wall;
+import Views.CoreView;
+import Views.HouseGraphic;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
 
 public class HouseTest {
   @Test
@@ -11,4 +18,26 @@ public class HouseTest {
     h.addRoom(r);
     assert h.getRooms().size() == 1;
   }
+
+  @Test
+  void autoLightsTests(){
+    CustomHouseXStream cxs = new CustomHouseXStream();
+    House testHouse = (House) cxs.fromXML(new File("House.xml"));
+    HouseGraphic hg = new HouseGraphic(testHouse);
+    UserProfileModel testUser = new UserProfileModel(ProfileType.ADULT, "test", 1);
+    EnvironmentModel.resetInstance();
+    CoreController SHC = new CoreController(new CoreView(), EnvironmentModel.createSimulation(testHouse,hg, testUser));
+
+    Room testRoom = testHouse.getRooms().get(0);
+
+    //No one is in the room
+    assert !testRoom.getLightsOn();
+
+    //call to update method of Observer that is usually automatically done by the EnvironmentModel.
+    SHC.update(testUser.getRoomID(), testRoom.getId());
+
+    //Room Light should be on now
+    assert testRoom.getLightsOn();
+  }
+
 }
