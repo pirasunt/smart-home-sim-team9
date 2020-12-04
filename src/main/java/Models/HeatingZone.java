33fb98f2dfffc7f17ffb44efdec88949eb100649
Patrick <patrickcgodbout@gmail.com>
2020-12-03 21:38:47 -0500

    added handling of ac/windows for summer season

--- src/main/java/Models/HeatingZone.java
+++ src/main/java/Models/HeatingZone.java
@@ -7,165 +7,175 @@ import Models.Walls.WindowWall;
 import Views.CustomConsole;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.UUID;
 
 public class HeatingZone {
 
-    private int temperature;
-    private ArrayList<Room> rooms = new ArrayList<Room>();
-    private Date summerStart;
-    private Date winterStart;
-    private boolean acOn;
-    private boolean heaterOn;
-    private String name;
+  private int temperature;
+  private final ArrayList<Room> rooms = new ArrayList<Room>();
+  private final Date summerStart;
+  private final Date winterStart;
+  private boolean acOn;
+  private boolean heaterOn;
+  private final String name;
 
-    public HeatingZone(Room[] rooms, Date summerStart, Date winterStart, String name) {
-        int total = 0;
-        int count = 0;
-        for (Room room : rooms) {
-            this.rooms.add(room);
-            room.setIsInHeatingZone(true);
-            total += room.getTemperature();
-            total++;
-        }
-        this.summerStart = summerStart;
-        this.winterStart = winterStart;
-        this.acOn = false;
-        this.heaterOn = false;
-        this.name = name;
-
-        setTemperature(EnvironmentModel.getOutsideTemp());
+  public HeatingZone(Room[] rooms, Date summerStart, Date winterStart, String name) {
+    int total = 0;
+    int count = 0;
+    for (Room room : rooms) {
+      this.rooms.add(room);
+      room.setIsInHeatingZone(true);
+      total += room.getTemperature();
+      total++;
     }
+    this.summerStart = summerStart;
+    this.winterStart = winterStart;
+    this.acOn = false;
+    this.heaterOn = false;
+    this.name = name;
 
-    public void setTemperature(int newTemp) {
+    setTemperature(EnvironmentModel.getOutsideTemp());
+  }
 
-        HeatingZone zone = this;
+  /**
+   * Verify if the context time is currently in the summer
+   *
+   * @return true if it is summer, false if it is winter.
+   */
+  private boolean isSummer() {
+      return Context.getDateObject().after(summerStart) && Context.getDateObject().before(winterStart);
+  }
 
-        Timer timer = new Timer(1000, new ActionListener() {
+  /**
+   * Verify if the temperature outside is hotter than the temperature inside
+   *
+   * @return true if it is hotter outside, false if it is colder outside.
+   */
+  private boolean hotOutside() {
+      return EnvironmentModel.getOutsideTemp() > this.temperature;
+  }
 
-            @Override
-            public void actionPerformed(ActionEvent e) {
+  /**
+   * Verify if we want to increase of decrease the temperature in the house.
+   *
+   * @param desiredTemp the temperature desired.
+   * @return true if the current temperature is higher than desired, false if not.
+   */
+  private boolean hotInside(int desiredTemp) {
+      return this.temperature > desiredTemp;
+  }
+
+  private void openAllWindowsInZone() {
+    for (Room room : rooms) {
+      for (Wall wall : room.getAllWalls()) {
+        if (wall.getType() == WallType.WINDOWS) {
+          if (EnvironmentModel.getSimulationRunning()) ((WindowWall) wall).openWindow();
+          else ((WindowWall) wall).setWindowOpen(true);
+        }
+      }
+    }
+  }
+
+  private void closeAllWindowsInZone() {
+    for (Room room : rooms) {
+      for (Wall wall : room.getAllWalls()) {
+        if (wall.getType() == WallType.WINDOWS) {
+          if (EnvironmentModel.getSimulationRunning()) ((WindowWall) wall).closeWindow();
+          else ((WindowWall) wall).setWindowOpen(false);
+        }
+      }
+    }
+  }
+
+  public void addRoom(Room room) {
+    try {
+      if (rooms.contains(room)) {
+        throw new RoomExistsInHeatingZoneException();
+      }
+      rooms.add(room);
+      room.setIsInHeatingZone(true);
+    } catch (RoomExistsInHeatingZoneException e) {
+    }
+  }
+
+  public void removeRoom(Room room) {
+    rooms.remove(room);
+    room.setIsInHeatingZone(false);
+  }
+
+  public int getTemperature() {
+    return this.temperature;
+  }
+
+  public void setTemperature(int newTemp) {
+
+    HeatingZone zone = this;
+
+    Timer timer =
+        new Timer(
+            1000,
+            new ActionListener() {
+
+              @Override
+              public void actionPerformed(ActionEvent e) {
                 if (zone.getTemperature() > newTemp) {
-                    while (zone.getTemperature() > newTemp) {
-                        zone.decrementTemperature();
-                    }
-                    ((Timer)e.getSource()).stop();
+                  while (zone.getTemperature() > newTemp) {
+                    zone.decrementTemperature();
+                  }
+                  ((Timer) e.getSource()).stop();
+                } else if (zone.getTemperature() < newTemp) {
+                  while (zone.getTemperature() < newTemp) {
+                    zone.incrementTemperature();
+                  }
+                  ((Timer) e.getSource()).stop();
                 }
-                else if (zone.getTemperature() < newTemp) {
-                    while (zone.getTemperature() < newTemp) {
-                        zone.incrementTemperature();
-                    }
-                    ((Timer)e.getSource()).stop();
+
+                // This handles opening and closing of windows/ac in summer
+                if (isSummer() && hotOutside()) {
+                  if (!acOn) {
+                    acOn = true;
+                    heaterOn = false;
+                    closeAllWindowsInZone();
+                    CustomConsole.print("The current temperature in zone :" + zone.getName() + " is " + zone.getTemperature());
+                    CustomConsole.print("The outside temperature is hotter and so the AC has been turned on, and windows closed.");
+                  }
+                } else if (isSummer() && !hotOutside()) {
+                  if (acOn) {
+                    acOn = false;
+                    heaterOn = false;
+                    openAllWindowsInZone();
+                    CustomConsole.print("The current temperature in zone :" + zone.getName() + " is " + zone.getTemperature());
+                    CustomConsole.print("The outside temperature is colder and so the AC has been turned off, and windows opened.");
+                  }
                 }
-            }
-        });
+              }
+            });
 
-        timer.start();
-    }
+    timer.start();
+  }
 
-    /**
-     * Verify if the context time is currently in the summer
-     * @return true if it is summer, false if it is winter.
-     */
-    private boolean isSummer() {
-        if (Context.getDateObject().after(summerStart) && Context.getDateObject().before(winterStart)) {
-            return true;
-        }
-        return false;
+  public void incrementTemperature() {
+    this.temperature++;
+    for (Room room : rooms) {
+      room.setTemperature(room.getTemperature() + 1);
     }
+  }
 
-    /**
-     * Verify if the temperature outside is hotter than the temperature inside
-     * @return true if it is hotter outside, false if it is colder outside.
-     */
-    private boolean hotOutside() {
-        if (EnvironmentModel.getOutsideTemp() > this.temperature) {
-            return true;
-        }
-        return false;
+  public void decrementTemperature() {
+    this.temperature--;
+    for (Room room : rooms) {
+      room.setTemperature(room.getTemperature() - 1);
     }
+  }
 
-    /**
-     * Verify if we want to increase of decrease the temperature in the house.
-     * @param desiredTemp the temperature desired.
-     * @return true if the current temperature is higher than desired, false if not.
-     */
-    private boolean hotInside(int desiredTemp) {
-        if (this.temperature > desiredTemp) {
-            return true;
-        }
-        return false;
-    }
+  public String getName() {
+    return this.name;
+  }
 
-    private void openAllWindowsInZone() {
-        for (Room room : rooms) {
-            for (Wall wall : room.getAllWalls()) {
-                if (wall.getType() == WallType.WINDOWS) {
-                    if (EnvironmentModel.getSimulationRunning())
-                        ((WindowWall)wall).openWindow();
-                    else
-                        ((WindowWall)wall).setWindowOpen(true);
-                }
-            }
-        }
-    }
-
-    private void closeAllWindowsInZone() {
-        for (Room room : rooms) {
-            for (Wall wall : room.getAllWalls()) {
-                if (wall.getType() == WallType.WINDOWS) {
-                    if (EnvironmentModel.getSimulationRunning())
-                        ((WindowWall) wall).closeWindow();
-                    else ((WindowWall) wall).setWindowOpen(false);
-                }
-            }
-        }
-    }
-
-    public void addRoom(Room room) {
-        try {
-            if (rooms.contains(room)) {
-                throw new RoomExistsInHeatingZoneException();
-            }
-            rooms.add(room);
-            room.setIsInHeatingZone(true);
-        }
-        catch (RoomExistsInHeatingZoneException e) {}
-    }
-
-    public void removeRoom(Room room) {
-        rooms.remove(room);
-        room.setIsInHeatingZone(false);
-    }
-
-    public int getTemperature() {
-        return this.temperature;
-    }
-
-    public void incrementTemperature() {
-        this.temperature++;
-        for (Room room : rooms) {
-            room.setTemperature(room.getTemperature() + 1);
-        }
-    }
-
-    public void decrementTemperature() {
-        this.temperature--;
-        for (Room room : rooms) {
-            room.setTemperature(room.getTemperature() - 1);
-        }
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public ArrayList<Room> getRooms() {
-        return this.rooms;
-    }
+  public ArrayList<Room> getRooms() {
+    return this.rooms;
+  }
 }
