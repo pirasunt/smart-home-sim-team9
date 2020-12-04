bc65428be26b7342e71e2bab04e2773d53a6c5da
Jason Botzas-Coluni <jason.botzas.coluni@gmail.com>
2020-12-03 23:26:32 -0500

    just ui and models

--- src/main/java/Models/HeatingModel.java
+++ src/main/java/Models/HeatingModel.java
@@ -1,23 +1,37 @@
 package Models;
 
+import javax.swing.*;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 
 public class HeatingModel {
 
-    private ArrayList<HeatingZone> heatingZones = new ArrayList<HeatingZone>();
-    private Date summerStart;
-    private Date winterStart;
-    private int topThreshold;
-    private int bottomThreshhold;
+  private final ArrayList<HeatingZone> heatingZones = new ArrayList<HeatingZone>();
+  private Date summerStart;
+  private Date winterStart;
+  private int topThreshold;
+  private int bottomThreshhold;
+  private final SpinnerNumberModel awayTempSpinner;
+  private final SpinnerNumberModel dangerTempSpinner;
 
-    public void createHeatingZone(Room[] rooms, String name) {
-        heatingZones.add(new HeatingZone(rooms, summerStart, winterStart, name));
-    }
+  public HeatingModel() {
+    awayTempSpinner = new SpinnerNumberModel();
+    dangerTempSpinner = new SpinnerNumberModel();
+  }
 
-    public ArrayList<HeatingZone> getHeatingZones() {
-        return this.heatingZones;
-    }
+  public SpinnerNumberModel getDangerTempSpinner() {
+    return dangerTempSpinner;
+  }
 
+  public void createHeatingZone(Room[] rooms, String name) {
+    heatingZones.add(new HeatingZone(rooms, summerStart, winterStart, name));
+  }
+
+  public SpinnerNumberModel getAwayTempSpinner() {
+    return this.awayTempSpinner;
+  }
+
+  public ArrayList<HeatingZone> getHeatingZones() {
+    return this.heatingZones;
+  }
 }
