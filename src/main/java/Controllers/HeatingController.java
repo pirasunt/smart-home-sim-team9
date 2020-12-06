0eb2b4653ea1d4b60fd7f4c54236b2f73636fcb9
Jason Botzas-Coluni <jason.botzas.coluni@gmail.com>
2020-12-05 19:47:57 -0500

    patty cake

--- src/main/java/Controllers/HeatingController.java
+++ src/main/java/Controllers/HeatingController.java
@@ -15,202 +15,202 @@ import java.util.Calendar;
 import java.util.Date;
 
 enum Season {
-  WINTER,
-  SUMMER,
+    WINTER,
+    SUMMER,
 }
 
 public class HeatingController {
 
-  private HeatingModel heatingModel;
-  private static HeatingModel sHeatingModel;
-  private HeatingModule heatingView;
-  private HeatZoneCreator heatZoneDialog;
+    private HeatingModel heatingModel;
+    private static HeatingModel sHeatingModel;
+    private HeatingModule heatingView;
+    private HeatZoneCreator heatZoneDialog;
 
-  public HeatingController(HeatingModel m, HeatingModule v) {
-    this.heatingModel = m;
-    this.heatingView = v;
-    sHeatingModel = m;
+    public HeatingController(HeatingModel m, HeatingModule v) {
+        this.heatingModel = m;
+        this.heatingView = v;
+        sHeatingModel = m;
 
-    heatingView.createHeatingZoneListener(new HeatingZoneCreationListener());
-    heatingView.initializeView(heatingModel.getSummerStart(), heatingModel.getWinterStart(), heatingModel.getAwayTempSpinner(), heatingModel.getDangerTempSpinner());
-    heatingView.createSummerChangeListener(new SummerChangeListener());
-    heatingView.createWinterChangeListener(new WinterChangeListener());
-  }
-
-  public static HeatingModel getStaticHeatingModel() {
-    return sHeatingModel;
-  }
-
-  public void createHeatingZone(String zoneName, Room[] rooms) {
-    heatingModel.createHeatingZone(rooms, zoneName);
-  }
-
-  public ArrayList<HeatingZone> getHeatingZones() {
-    return this.heatingModel.getHeatingZones();
-  }
-
-  public Room[] getAvailableRooms() {
-    ArrayList<Room> allRooms = Context.getHouse().getRooms();
-    ArrayList<Room> result = new ArrayList<>();
-
-    for (Room room : allRooms) {
-      if (isRoomAvailable(room)) {
-        result.add(room);
-      }
+        heatingView.createHeatingZoneListener(new HeatingZoneCreationListener());
+        heatingView.initializeView(heatingModel.getSummerStart(), heatingModel.getWinterStart(), heatingModel.getAwayTempSpinner(), heatingModel.getDangerTempSpinner());
+        heatingView.createSummerChangeListener(new SummerChangeListener());
+        heatingView.createWinterChangeListener(new WinterChangeListener());
     }
 
-    Room[] resultArr = new Room[result.size()];
-    return resultArr;
-  }
-
-  private boolean isRoomAvailable(Room room) {
-    for (HeatingZone zone : getHeatingZones()) {
-      if (zone.getRooms().contains(room)) {
-        return false;
-      }
+    public static HeatingModel getStaticHeatingModel() {
+        return sHeatingModel;
     }
 
-    return true;
-  }
-
-
-  private class HeatingZoneCreationListener implements ActionListener {
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-      heatZoneDialog = new HeatZoneCreator();
-      heatZoneDialog.addCancelButtonListener(new ZoneCancelButtonListener());
-      heatZoneDialog.addConfirmButtonListener(new ZoneConfirmButtonListener());
-
-      heatZoneDialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-      heatZoneDialog.setLocationRelativeTo(null);
-      heatZoneDialog.setVisible(true);
+    public void createHeatingZone(String zoneName, Room[] rooms) {
+        heatingModel.createHeatingZone(rooms, zoneName);
     }
 
-    private class ZoneCancelButtonListener implements ActionListener {
-      /**
-       * Invoked when an action occurs.
-       *
-       * @param e the event to be processed
-       */
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        heatZoneDialog.dispose();
-      }
+    public ArrayList<HeatingZone> getHeatingZones() {
+        return this.heatingModel.getHeatingZones();
     }
 
-    private class ZoneConfirmButtonListener implements ActionListener {
-      /**
-       * Invoked when an action occurs.
-       *
-       * @param e the event to be processed
-       */
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        if (heatZoneDialog.getZoneName().length()>0/*&& some rooms are selected*/) {
+    public Room[] getAvailableRooms() {
+        ArrayList<Room> allRooms = Context.getHouse().getRooms();
+        ArrayList<Room> result = new ArrayList<>();
 
-          //call controller.createHeatingZone(zoneName.getText(), rooms);
-
-          //refresh the SHH to display a list of all existing zones
-          //can maybe use an observer (?), not necessary though
-          //controller.getHeatingZones() should return all existing zones
-
-          //code to test without UI, dont forget to remove
-          Room[] testRooms = {Context.getHouse().getRooms().get(1), Context.getHouse().getRooms().get(2)};
-          createHeatingZone(heatZoneDialog.getZoneName(), testRooms);
-
-          System.out.println(getHeatingZones().get(0).getName());
-
-          heatZoneDialog.dispose();
-        } else
-          CustomConsole.print("Make sure to name the heating zone you are creating, as well as select at least 1 room.");
-      }
-    }
-  }
-
-  private class SummerChangeListener implements ActionListener {
-    /**
-     * Invoked when an action occurs.
-     *
-     * @param e the event to be processed
-     */
-    @Override
-    public void actionPerformed(ActionEvent e) {
-      heatingView.changeDate(new SeasonDateFormatter(Season.SUMMER));
-
-    }
-  }
-
-  private class WinterChangeListener implements ActionListener {
-    /**
-     * Invoked when an action occurs.
-     *
-     * @param e the event to be processed
-     */
-    @Override
-    public void actionPerformed(ActionEvent e) {
-      heatingView.changeDate(new SeasonDateFormatter(Season.WINTER));
-    }
-  }
-
-  private class SeasonDateFormatter extends JFormattedTextField.AbstractFormatter {
-
-
-    private final String datePattern = "MMMM dd";
-    private final SimpleDateFormat dateFormatter = new SimpleDateFormat(datePattern);
-    private Season seasonType;
-
-    public SeasonDateFormatter(Season type){
-      this.seasonType=type;
-    }
-
-    /**
-     * Parses <code>text</code> returning an arbitrary Object. Some
-     * formatters may return null.
-     *
-     * @param text String to convert
-     * @return Object representation of text
-     * @throws ParseException if there is an error in the conversion
-     */
-    @Override
-    public Object stringToValue(String text) throws ParseException {
-      Calendar cal = Calendar.getInstance();
-      cal.setTime((Date) dateFormatter.parseObject(text));
-      return cal;
-    }
-
-    /**
-     * Returns the string value to display for <code>value</code>.
-     *
-     * @param value Value to convert
-     * @return String representation of value
-     * @throws ParseException if there is an error in the conversion
-     */
-    @Override
-    public String valueToString(Object value) throws ParseException {
-      if (value != null) {
-        Calendar cal = (Calendar) value;
-        if(seasonType == Season.SUMMER) {
-          if(heatingModel.getWinterStart().getMonth() == cal.get(Calendar.MONTH) && heatingModel.getWinterStart().getDate() == cal.get(Calendar.DAY_OF_MONTH))
-            CustomConsole.print("ERROR: Winter and Summer Start Dates can not be the same. Please choose a different Date");
-          else {
-            heatingView.updateSummerStartLabel(dateFormatter.format(cal.getTime()));
-            heatingModel.updateSummerStart(cal.getTime());
-          }
-
-        } else if(seasonType == Season.WINTER){
-          if(heatingModel.getSummerStart().getMonth() == cal.get(Calendar.MONTH) && heatingModel.getSummerStart().getDate() == cal.get(Calendar.DAY_OF_MONTH))
-            CustomConsole.print("ERROR: Winter and Summer Start Dates can not be the same. Please choose a different Date");
-          else {
-            heatingView.updateWinterStartLabel(dateFormatter.format(cal.getTime()));
-            heatingModel.updateWinterStart(cal.getTime());
-          }
+        for (Room room : allRooms) {
+            if (isRoomAvailable(room)) {
+                result.add(room);
+            }
         }
-        heatingView.disposeDatePicker();
 
-        return dateFormatter.format(cal.getTime());
-      }
-      return "";
+        Room[] resultArr = new Room[result.size()];
+        return resultArr;
     }
-  }
+
+    private boolean isRoomAvailable(Room room) {
+        for (HeatingZone zone : getHeatingZones()) {
+            if (zone.getRooms().contains(room)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    private class HeatingZoneCreationListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            heatZoneDialog = new HeatZoneCreator();
+            heatZoneDialog.addCancelButtonListener(new ZoneCancelButtonListener());
+            heatZoneDialog.addConfirmButtonListener(new ZoneConfirmButtonListener());
+
+            heatZoneDialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+            heatZoneDialog.setLocationRelativeTo(null);
+            heatZoneDialog.setVisible(true);
+        }
+
+        private class ZoneCancelButtonListener implements ActionListener {
+            /**
+             * Invoked when an action occurs.
+             *
+             * @param e the event to be processed
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                heatZoneDialog.dispose();
+            }
+        }
+
+        private class ZoneConfirmButtonListener implements ActionListener {
+            /**
+             * Invoked when an action occurs.
+             *
+             * @param e the event to be processed
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (heatZoneDialog.getZoneName().length()>0/*&& some rooms are selected*/) {
+
+                    //call controller.createHeatingZone(zoneName.getText(), rooms);
+
+                    //refresh the SHH to display a list of all existing zones
+                    //can maybe use an observer (?), not necessary though
+                    //controller.getHeatingZones() should return all existing zones
+
+                    //code to test without UI, dont forget to remove
+                    Room[] testRooms = {Context.getHouse().getRooms().get(1), Context.getHouse().getRooms().get(2)};
+                    createHeatingZone(heatZoneDialog.getZoneName(), testRooms);
+
+                    System.out.println(getHeatingZones().get(0).getName());
+
+                    heatZoneDialog.dispose();
+                } else
+                    CustomConsole.print("Make sure to name the heating zone you are creating, as well as select at least 1 room.");
+            }
+        }
+    }
+
+    private class SummerChangeListener implements ActionListener {
+        /**
+         * Invoked when an action occurs.
+         *
+         * @param e the event to be processed
+         */
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            heatingView.changeDate(new SeasonDateFormatter(Season.SUMMER));
+
+        }
+    }
+
+    private class WinterChangeListener implements ActionListener {
+        /**
+         * Invoked when an action occurs.
+         *
+         * @param e the event to be processed
+         */
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            heatingView.changeDate(new SeasonDateFormatter(Season.WINTER));
+        }
+    }
+
+    private class SeasonDateFormatter extends JFormattedTextField.AbstractFormatter {
+
+
+        private final String datePattern = "MMMM dd";
+        private final SimpleDateFormat dateFormatter = new SimpleDateFormat(datePattern);
+        private Season seasonType;
+
+        public SeasonDateFormatter(Season type){
+            this.seasonType=type;
+        }
+
+        /**
+         * Parses <code>text</code> returning an arbitrary Object. Some
+         * formatters may return null.
+         *
+         * @param text String to convert
+         * @return Object representation of text
+         * @throws ParseException if there is an error in the conversion
+         */
+        @Override
+        public Object stringToValue(String text) throws ParseException {
+            Calendar cal = Calendar.getInstance();
+            cal.setTime((Date) dateFormatter.parseObject(text));
+            return cal;
+        }
+
+        /**
+         * Returns the string value to display for <code>value</code>.
+         *
+         * @param value Value to convert
+         * @return String representation of value
+         * @throws ParseException if there is an error in the conversion
+         */
+        @Override
+        public String valueToString(Object value) throws ParseException {
+            if (value != null) {
+                Calendar cal = (Calendar) value;
+                if(seasonType == Season.SUMMER) {
+                    if(heatingModel.getWinterStart().getMonth() == cal.get(Calendar.MONTH) && heatingModel.getWinterStart().getDate() == cal.get(Calendar.DAY_OF_MONTH))
+                        CustomConsole.print("ERROR: Winter and Summer Start Dates can not be the same. Please choose a different Date");
+                    else {
+                        heatingView.updateSummerStartLabel(dateFormatter.format(cal.getTime()));
+                        heatingModel.updateSummerStart(cal.getTime());
+                    }
+
+                } else if(seasonType == Season.WINTER){
+                    if(heatingModel.getSummerStart().getMonth() == cal.get(Calendar.MONTH) && heatingModel.getSummerStart().getDate() == cal.get(Calendar.DAY_OF_MONTH))
+                        CustomConsole.print("ERROR: Winter and Summer Start Dates can not be the same. Please choose a different Date");
+                    else {
+                        heatingView.updateWinterStartLabel(dateFormatter.format(cal.getTime()));
+                        heatingModel.updateWinterStart(cal.getTime());
+                    }
+                }
+                heatingView.disposeDatePicker();
+
+                return dateFormatter.format(cal.getTime());
+            }
+            return "";
+        }
+    }
 }
